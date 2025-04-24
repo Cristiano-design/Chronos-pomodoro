78f72ca64961966c4465a787c92ff7e0af769474
Cristiano Ferreira <tfcriskds@gmail.com>
2025-04-24 00:42:50 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <h1>Olá mundo</h1>
-      <p>Lorem ipsum</p>
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu sem
+        sodales, dignissim ipsum eget, dignissim erat. Mauris sodales quis
+        lectus eget congue. Nulla non fringilla ante. Praesent in blandit mi.
+        Aenean semper, diam vitae malesuada rhoncus, lacus augue fermentum
+        dolor, quis vulputate nunc nisi non turpis. Praesent malesuada ante
+        tellus, in gravida mi placerat ac. Nulla orci urna, posuere posuere
+        posuere ac, ultrices ullamcorper eros. Vivamus pellentesque tempor
+        dictum. Cras euismod laoreet nisl vitae aliquam.
+      </p>
     </>
   );
 }

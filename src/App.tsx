87b88563d505a665c97cc34ce2a231514135cf40
Cristiano Ferreira <tfcriskds@gmail.com>
2025-05-26 20:56:16 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
+
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2='String'>
-        Olá mundo 1
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu sem
-        sodales, dignissim ipsum eget, dignissim erat. Mauris sodales quis
-        lectus eget congue. Nulla non fringilla ante. Praesent in blandit mi.
-        Aenean semper, diam vitae malesuada rhoncus, lacus augue fermentum
-        dolor, quis vulputate nunc nisi non turpis. Praesent malesuada ante
-        tellus, in gravida mi placerat ac. Nulla orci urna, posuere posuere
-        posuere ac, ultrices ullamcorper eros. Vivamus pellentesque tempor
-        dictum. Cras euismod laoreet nisl vitae aliquam.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }

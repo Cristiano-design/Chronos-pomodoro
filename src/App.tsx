495f34f8ed2a7c1f03bb8690e72796c8e614e131
cristiano ferreira <tfcriskds@gmail.com>
2025-09-22 10:25:00 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,13 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
+
 
 export function App() {
-  const numero = 0;
+
+
   return (
     <>
-
-      <Heading>Numero: {numero}</Heading>
-
       <Container>
         <Logo />
       </Container>
@@ -35,7 +33,7 @@ export function App() {
         <form action='' className='form'>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='task'
               id='input'
               type='text'
               placeholder='Digite algo'
